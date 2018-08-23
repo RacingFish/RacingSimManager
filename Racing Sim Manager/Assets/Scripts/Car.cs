0f341588cf0099e044f6624e9efda5d9cb7f7e98
RacingFish <42640638+RacingFish@users.noreply.github.com>
2018-08-23 12:51:03 +0100

    New Scripts - Car and RaceManager
    Added new car and racemanager scripts to the project.

---
+++ Racing Sim Manager/Assets/Scripts/Car.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour {
+
+	public float engineValue = 0f;
+	public float aeroValue = 0f;
+	public float chassisValue = 0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Racing Sim Manager/Assets/Scripts/RaceManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaceManager : MonoBehaviour 
+{
+	enum RaceWeekendState 
+	{
+		RaceWeekendState_LoadTrack,
+		RaceWeekendState_Practice,
+		RaceWeekendState_Qualifying,
+		RaceWeekendState_Race,
+		RaceWeekendState_RaceOver
+	}
+
+	RaceWeekendState raceWeekendState;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		switch(raceWeekendState)
+		{
+			case RaceWeekendState.RaceWeekendState_LoadTrack:
+			break;
+			case RaceWeekendState.RaceWeekendState_Practice:
+			break;
+			case RaceWeekendState.RaceWeekendState_Qualifying:
+			break;
+			case RaceWeekendState.RaceWeekendState_Race:
+			break;
+			case RaceWeekendState.RaceWeekendState_RaceOver:
+			break;
+		}
+	}
+
+	void SetRaceWeekendState (RaceWeekendState eState) 
+	{
+		Debug.Log (eState);
+		raceWeekendState = eState;
+	}
+}
